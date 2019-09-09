570b3d34bf7ecc9322ed70a0bbaa756ec8b04762
tzhenkai <sglancelot@gmail.com>
2019-09-10 00:03:19 +0800

    Added static front-end build files. Testing to heroku

---
+++ build/precache-manifest.41c918f7d83cbedbe281a63eeab27aeb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99eaf9ef65ca3689c9137c920eea74ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a79064231432b802593",
+    "url": "/static/js/2.0395057b.chunk.js"
+  },
+  {
+    "revision": "0a3e124d921d45b3d31d",
+    "url": "/static/js/main.8821e7a9.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
