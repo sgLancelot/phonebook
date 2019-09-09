3e1caf82636bab7f386a15805b2f4594e5a0b02f
tzhenkai <sglancelot@gmail.com>
2019-09-09 23:23:35 +0800

    Preparing to deploy to heroku @ part 3b

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
 app.use(bodyParser.json())
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 morgan.token('data', (req) => {
@@ -76,7 +78,7 @@ app.post('/api/persons', (req,res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
